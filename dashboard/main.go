06762e1f83267a23b412dd5b8e3e10cdb7edf6d3
Íñigo Aréjula Aísa <arejula27@gmail.com>
2024-10-03 01:50:47 +0200

    change time format

--- dashboard/main.go
+++ dashboard/main.go
@@ -44,7 +44,7 @@ func getData() string {
 		TrumpPopularity  int    `json:"trumpPopularity"`
 		HarrisPopularity int    `json:"harrisPopularity"`
 	}
-	currentTime := time.Now().Format("2006-01-02 15:04:05")
+	currentTime := time.Now().Format("15:04:05")
 	res, err := json.Marshal(result{Timestamps: currentTime, TrumpPopularity: trumpSupport, HarrisPopularity: harrisSupport})
 	if err != nil {
 		return "{}"
