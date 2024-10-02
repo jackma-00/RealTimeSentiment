d79d883e13c6e76b8877acfede0e8bedc9e08fd3
Íñigo Aréjula Aísa <arejula27@gmail.com>
2024-10-02 16:49:47 +0200

    consumes data from mongo

--- dashboard/main.go
+++ dashboard/main.go
@@ -1,18 +1,55 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"time"
+
 	"github.com/arejula27/data-intensive-demo/dashboard/view"
 	"github.com/labstack/echo/v4"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-// HistoryData is a struct that holds the data for the history of Trump's popularity
-type TimeSeries struct {
-	timestamp       string
-	TrumpPopularity float64
+var trump_db *mongo.Collection
+var harris_db *mongo.Collection
+
+type candidateData struct {
+	Timestamp time.Time `bson:"timestamp"`
+	Support   int       `bson:"support"`
+	Oppose    int       `bson:"oppose"`
 }
 
 func getData() ([]string, []int) {
-	return []string{"1", "2", "3", "4", "5"}, []int{20, 50, 70, 40, 55}
+	cursor, err := trump_db.Find(context.TODO(), bson.D{})
+	if err != nil {
+		panic(err)
+	}
+
+	var trumpResults []candidateData
+	if err = cursor.All(context.TODO(), &trumpResults); err != nil {
+		panic(err)
+	}
+	var harrisResults []candidateData
+	cursor, err = harris_db.Find(context.TODO(), bson.D{})
+	if err != nil {
+		panic(err)
+	}
+	if err = cursor.All(context.TODO(), &harrisResults); err != nil {
+		panic(err)
+	}
+
+	//iterate over the results create a slice with the timestamps and a slice with probablity of trump winning
+	var timestamps []string
+	var trump []int
+	for i, result := range trumpResults {
+		timestamps = append(timestamps, fmt.Sprintf("%d", i))
+		if result.Support+result.Oppose != 0 {
+			trump = append(trump, result.Support/(result.Support+result.Oppose)*100)
+		}
+	}
+	return timestamps, trump
 }
 
 func IndexPageHandler(c echo.Context) error {
@@ -22,6 +59,22 @@ func IndexPageHandler(c echo.Context) error {
 }
 
 func main() {
+	//connect to mongoDB
+
+	uri := "mongodb://localhost:27017"
+	client, err := mongo.Connect(context.TODO(), options.Client().
+		ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err := client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+	trump_db = client.Database("usa2024").Collection("trump")
+	harris_db = client.Database("usa2024").Collection("harris")
+
 	e := echo.New()
 	e.GET("/", IndexPageHandler)
 	e.Logger.Fatal(e.Start(":1323"))
