1b1a391954d115c271bca4bf0a322830d653c6a5
Íñigo Aréjula Aísa <arejula27@gmail.com>
2024-10-02 23:20:23 +0200

    remove unused dependencies

--- spark/classify_tweets.py
+++ spark/classify_tweets.py
@@ -52,10 +52,7 @@ def classify_trump(batch_df: DataFrame, batch_id: int):
 spark = SparkSession.builder \
     .appName("KafkaSparkStreamingJSON") \
     .config("spark.jars.packages",
-            "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.2,"
-            "org.mongodb.spark:mongo-spark-connector_2.12:3.0.1") \
-    .config("spark.mongodb.write.connection.uri",
-            "mongodb://127.0.0.1:27017/mydb.usa2024") \
+            "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.2,") \
     .getOrCreate()
 
 df = spark.readStream.format("kafka") \
