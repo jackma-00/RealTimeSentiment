018246c8db6a90a40d9256c1076b197411a263e4
jackma-00 <jacopo.maragna1@gmail.com>
2024-10-05 14:38:26 +0200

    Add Docker

--- consumer/src/sentiment_analysis/main.py
+++ consumer/src/sentiment_analysis/main.py
@@ -6,11 +6,11 @@ from politics import SentimentAnalyzer
 def main():
     # Load the tweets from the CSV file
     df = pd.read_csv("src/data/tweets.csv")
-    
+
     # Initialize the TweetParser and SentimentAnalyzer
     parser = TweetParser()
     analyzer = SentimentAnalyzer()
-    
+
     # Parse the tweets
     parsed_tweets = parser.parse_tweets_as_df(df, label="__label__trump")
 
@@ -21,6 +21,7 @@ def main():
     for i in range(5):
         tweet = parsed_tweets.iloc[i]
         analyzer.print_results(tweet["tweet"])
+
 
 if __name__ == "__main__":
     main()
